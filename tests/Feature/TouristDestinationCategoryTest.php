9e56e61c49e56279d4ec7eccf3355718319ff49a
ganiadiw <ganiadiw@gmail.com>
2022-12-26 22:59:05 +0700

    test: add new test

--- tests/Feature/TouristDestinationCategoryTest.php
+++ tests/Feature/TouristDestinationCategoryTest.php
@@ -15,4 +15,13 @@ class TouristDestinationCategoryTest extends TestCase
         $response->assertStatus(200);
         $response->assertSeeText('Kelola Data Kategori Destinasi Wisata');
     }
+
+    public function test_a_create_page_can_be_rendered()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('tourist-destination-categories.create'));
+        $response->assertStatus(200);
+        $response->assertSeeText('Tambah Data Kategori Destinasi Wisata');
+    }
 }
