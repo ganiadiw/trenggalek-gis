dd8b290bbd1f5e9a33199eec1794ee4446924ab5
ganiadiw <ganiadiw@gmail.com>
2022-12-26 22:45:36 +0700

    delete unused import

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -9,7 +9,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
-use Laravolt\Avatar\Facade as Avatar;
 
 class UserController extends Controller
 {
