91586d373c51e9b4b43d68a4220c9e4cb68d7480
ganiadiw <ganiadiw@gmail.com>
2022-12-26 22:46:54 +0700

    update style and dropdown menu

--- resources/views/components/side-link.blade.php
+++ resources/views/components/side-link.blade.php
@@ -1,17 +1,18 @@
-@props(['active', 'svgIcon' => null])
+@props(['active', 'svgIcon' => null , 'spanClasses' => 'w-1 bg-blue-500 rounded-tr-lg rounded-br-lg', 'titleClasses' => 'ml-4'])
 
 @php
     $spanClasses = ($active ?? false)
-                ? 'w-1 bg-blue-500 rounded-tr-lg rounded-br-lg'
+                ? $spanClasses
                 : 'hidden';
 
     $classes = ($active ?? false)
-                ? 'flex items-center w-full h-12 pl-4 hover:bg-gray-300 hover:bg-white text-black font-bold'
-                : 'flex items-center w-full h-12 pl-4 hover:bg-gray-300 mx-1 hover:rounded-lg hover:text-gray-900'
+                ? 'flex items-center w-full h-12 pl-4 my-1 text-black font-bold'
+                : 'flex items-center w-full h-12 pl-4 hover:bg-gray-300 my-1 mx-1 hover:rounded-md hover:text-gray-900';
+
 @endphp
 
 <span {{ $attributes->merge(['class' => $spanClasses]) }}></span>
 <a {{ $attributes->merge(['class' => $classes]) }}>
     {{ $svgIcon }}
-    <span class="ml-4">{{ $title }}</span>
+    <span {{ $attributes->merge(['class' => $titleClasses]) }}>{{ $title }}</span>
 </a>
