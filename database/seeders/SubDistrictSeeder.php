333eabf71d954f432a8a2e56424ee71c8cfa587e
ganiadiw <ganiadiw@gmail.com>
2023-01-24 01:40:09 +0700

    delete unused import

--- database/seeders/SubDistrictSeeder.php
+++ database/seeders/SubDistrictSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\SubDistrict;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class SubDistrictSeeder extends Seeder
 {
@@ -15,8 +14,6 @@ class SubDistrictSeeder extends Seeder
      */
     public function run()
     {
-        // $sql = file_get_contents(database_path().'/seeders/sql_files/sub_district.sql');
-        // DB::unprepared($sql);
         SubDistrict::create([
             'code' => '3503000',
             'name' => 'sample',
@@ -24,7 +21,7 @@ class SubDistrictSeeder extends Seeder
             'longitude' => '111.4543148316443',
             'geojson_path' => 'public/geojson/PA-lkdYD-3503010.geojson',
             'geojson_name' => 'PA-lkdYD-3503010.geojson',
-            'fill_color' => '#C7B35A'
+            'fill_color' => '#C7B35A',
         ]);
     }
 }
--- database/seeders/TouristDestinationSeeder.php
+++ database/seeders/TouristDestinationSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\TouristDestination;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TouristDestinationSeeder extends Seeder
