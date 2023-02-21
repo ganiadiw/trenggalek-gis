d3148e8b05e51524527e3f6f4d98ec844afe2c10
ganiadiw <ganiadiw@gmail.com>
2023-02-21 15:14:04 +0700

    modify validation method

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,26 +10,26 @@ class ImageController extends Controller
 {
     public function store(Request $request)
     {
-        $this->validate($request, [
+        $request->validate([
             'image' => ['image', 'mimes:png,jpg,jpeg,gif'],
         ]);
 
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $filename = $image->getClientOriginalName();
-            $folder = 'public/tmp/media/images';
-            $path = $image->storeAs($folder, $filename);
+        $image = $request->file('image');
+        $filename = $image->getClientOriginalName();
+        $folder = 'public/tmp/media/images';
+        $path = $image->storeAs($folder, $filename);
 
-            TemporaryFile::create([
-                'foldername' => $folder,
-                'filename' => $filename,
-            ]);
+        TemporaryFile::create([
+            'foldername' => $folder,
+            'filename' => $filename,
+        ]);
 
-            return response()->json([
-                'location' => Storage::url($path),
-                'filename' => $filename,
-            ]);
-        }
+        return response()->json([
+            'location' => Storage::url($path),
+            'filename' => $filename,
+        ]);
+        // if ($request->file('image')) {
+        // }
     }
 
     public function destroy($filename)
