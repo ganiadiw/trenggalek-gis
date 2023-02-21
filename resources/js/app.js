fed79761c38adee715b9ac856e7c277e0a8b96c8
ganiadiw <ganiadiw@gmail.com>
2023-02-21 15:13:29 +0700

    add alpine js plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,10 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import persist from '@alpinejs/persist';
+import intersect from "@alpinejs/intersect";
 
 window.Alpine = Alpine;
 Alpine.plugin(persist);
+Alpine.plugin(intersect);
 
 Alpine.start();
