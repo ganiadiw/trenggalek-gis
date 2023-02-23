c46dd0641204e94682e33ee98a7045ae7b340802
ganiadiw <ganiadiw@gmail.com>
2023-02-23 23:46:42 +0700

    change tab identation

--- resources/views/js/image-preview.blade.php
+++ resources/views/js/image-preview.blade.php
@@ -1,12 +1,12 @@
 <script>
-        function imagePreview(imagePreviewTagName, imageFile, event) {
-            imageFile.addEventListener(event, function () {
-                const file = this.files[0];
+    function imagePreview(imagePreviewTagName, imageFile, event) {
+        imageFile.addEventListener(event, function () {
+            const file = this.files[0];
 
-                if (file.type.match(/image\/*/)) {
-                    const url = URL.createObjectURL(file);
-                    imagePreviewTagName.src = url;
-                }
-            });
-        }
+            if (file.type.match(/image\/*/)) {
+                const url = URL.createObjectURL(file);
+                imagePreviewTagName.src = url;
+            }
+        });
+    }
 </script>
