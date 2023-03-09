412d03d96c1286ce9f26eb0ae931c7a048b9747d
ganiadiw <ganiadiw@gmail.com>
2023-03-09 08:45:44 +0700

    test: add missing test for update data

--- tests/Feature/TouristDestination/UpdateTouristDestinationTest.php
+++ tests/Feature/TouristDestination/UpdateTouristDestinationTest.php
@@ -131,6 +131,116 @@ class UpdateTouristDestinationTest extends TestCase
         ]);
     }
 
+    public function test_an_authenticated_user_can_update_tourist_destination_with_image_in_description_editor_and_remove_old_image_if_no_longer_used()
+    {
+        // Preparation
+        $image1 = UploadedFile::fake()->image('image1678273485413.png');
+        $image2 = UploadedFile::fake()->image('image1678273485552.png');
+        $this->touristDestination->addMedia($image1)->toMediaCollection('tourist-destinations');
+        $this->touristDestination->addMedia($image2)->toMediaCollection('tourist-destinations');
+
+        $this->assertTrue(Storage::exists('public/media/1/image1678273485413.png'));
+        $this->assertTrue(Storage::exists('public/media/2/image1678273485552.png'));
+
+        $response = $this->actingAs($this->user)->put('/dashboard/tourist-destinations/' . $this->touristDestination->slug, [
+            'sub_district_id' => 1,
+            'tourist_destination_category_id' => 1,
+            'name' => 'Pantai Konang Indah',
+            'manager' => 'LDMH',
+            'address' => 'Dusun Nglebeng, Desa Nglebeng, Kecamatan Panggul',
+            'description' => '<p>Terkenal dengan keindahan pantai dan kuliner olahan hasil laut</p>',
+            'distance_from_city_center' => '56 KM',
+            'transportation_access' => 'Bisa diakses dengan bus, mobil, dan sepeda motor',
+            'facility' => 'MCK, Mushola, Lahan Parkir, Food Court',
+            'latitude' => -8.27466803,
+            'longitude' => 111.45297354,
+            'media_files' => json_encode([
+                'used_images' => [
+                    [
+                        'filename' => 'image1678273485413.png'
+                    ],
+                ],
+                'unused_images' => [
+                    [
+                        'filename' => 'image1678273485552.png'
+                    ],
+                ],
+            ]),
+        ]);
+        $response->assertValid();
+        $response->assertRedirect(url()->previous());
+        $response->assertSessionHasNoErrors();
+        $tourisDestination = TouristDestination::first();
+        $this->assertDatabaseHas('tourist_destinations', [
+            'name' => 'Pantai Konang Indah',
+            'latitude' => -8.27466803,
+            'longitude' => 111.45297354,
+        ]);
+        // Use Spatie Media Libary Package
+        $this->assertDatabaseHas('media', [
+            'model_id' => $tourisDestination->id,
+            'collection_name' => 'tourist-destinations',
+            'file_name' => 'image1678273485413.png'
+        ]);
+        $this->assertDatabaseMissing('media', [
+            'model_id' => $tourisDestination->id,
+            'collection_name' => 'tourist-destinations',
+            'file_name' => 'image1678273485552.png'
+        ]);
+        $this->assertFalse(Storage::exists('public/media/2/image1678273485552.png'));
+    }
+
+    public function test_an_authenticated_user_can_update_tourist_destination_with_add_new_image_in_description_editor()
+    {
+        $this->actingAs($this->user)->postJson('/dashboard/images', [
+            'image' => UploadedFile::fake()->image('image1678273485732.png'),
+        ]);
+        $this->assertDatabaseHas('temporary_files', [
+            'foldername' => 'public/tmp/media/images',
+            'filename' => 'image1678273485732.png',
+        ]);
+
+        $this->assertTrue(Storage::exists('public/tmp/media/images/image1678273485732.png'));
+
+        $response = $this->actingAs($this->user)->put('/dashboard/tourist-destinations/' . $this->touristDestination->slug, [
+            'sub_district_id' => 1,
+            'tourist_destination_category_id' => 1,
+            'name' => 'Pantai Konang Indah',
+            'manager' => 'LDMH',
+            'address' => 'Dusun Nglebeng, Desa Nglebeng, Kecamatan Panggul',
+            'description' => '<p>Terkenal dengan keindahan pantai dan kuliner olahan hasil laut</p>',
+            'distance_from_city_center' => '56 KM',
+            'transportation_access' => 'Bisa diakses dengan bus, mobil, dan sepeda motor',
+            'facility' => 'MCK, Mushola, Lahan Parkir, Food Court',
+            'latitude' => -8.27466803,
+            'longitude' => 111.45297354,
+            'media_files' => json_encode([
+                'used_images' => [
+                    [
+                        'filename' => 'image1678273485732.png'
+                    ],
+                ],
+                'unused_images' => null,
+            ]),
+        ]);
+        $response->assertValid();
+        $response->assertRedirect(url()->previous());
+        $response->assertSessionHasNoErrors();
+        $tourisDestination = TouristDestination::first();
+        $this->assertDatabaseHas('tourist_destinations', [
+            'name' => 'Pantai Konang Indah',
+            'latitude' => -8.27466803,
+            'longitude' => 111.45297354,
+        ]);
+        // Use Spatie Media Libary Package
+        $this->assertDatabaseHas('media', [
+            'model_id' => $tourisDestination->id,
+            'collection_name' => 'tourist-destinations',
+            'file_name' => 'image1678273485732.png'
+        ]);
+        $this->assertFalse(Storage::exists('public/tmp/media/images/image1678273485732.png'));
+    }
+
     public function test_a_guest_cannot_update_new_tourist_destination()
     {
         $response = $this->put('/dashboard/tourist-destinations/' . $this->touristDestination->slug, [
