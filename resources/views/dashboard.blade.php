2cfbbfa5808c1edfe14401282e4babee7961586b
ganiadiw <ganiadiw@gmail.com>
2023-04-29 23:17:31 +0700

    add statistic chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,7 @@
                     @can('view_superadmin_statistic')
                         <x-statistic-card>
                             <x-slot name="svgIcon">
-                                <div
-                                    class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
+                                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
                                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                         <path
                                             d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
@@ -22,8 +21,7 @@
                     @endcan
                     <x-statistic-card>
                         <x-slot name="svgIcon">
-                            <div
-                                class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
+                            <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                     fill="currentColor" class="w-5 h-5 bi bi-map-fill" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd"
@@ -36,8 +34,24 @@
                     </x-statistic-card>
                     <x-statistic-card>
                         <x-slot name="svgIcon">
-                            <div
-                                class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
+                            <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-category-2"
+                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M14 4h6v6h-6z"></path>
+                                    <path d="M4 14h6v6h-6z"></path>
+                                    <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                                    <path d="M7 7m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                                </svg>
+                            </div>
+                        </x-slot>
+                        <x-slot name="title">Total Kategori Wisata</x-slot>
+                        <x-slot name="value">{{ count($categories) }}</x-slot>
+                    </x-statistic-card>
+                    <x-statistic-card>
+                        <x-slot name="svgIcon">
+                            <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                     fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                                     <path
@@ -48,71 +62,23 @@
                         <x-slot name="title">Total Destinasi Wisata</x-slot>
                         <x-slot name="value">{{ count($touristDestinations) }}</x-slot>
                     </x-statistic-card>
-                    <x-statistic-card>
-                        <x-slot name="svgIcon">
-                            <div
-                                class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                                    <path
-                                        d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
-                                </svg>
-                            </div>
-                        </x-slot>
-                        <x-slot name="title">Total Desa Wisata</x-slot>
-                        <x-slot name="value">17</x-slot>
-                    </x-statistic-card>
                 </div>
             </div>
-            <div x-data="{ current: $persist(1) }" class="p-3 bg-white border-2 rounded-md shadow-lg border-slate-300">
+            <x-head.leaflet-init class="w-full rounded-lg h-128" />
+            <div class="grid w-full gap-4 p-4 pb-10 mt-5 bg-white rounded-md lg:grid-cols-2">
                 <div>
-                    <ul class="flex space-x-5 text-sm text-center text-gray-400 border-b">
-                        <li>
-                            <button @click="current = 1" class="inline-flex items-center px-6 py-2 hover:text-gray-600"
-                                x-bind:class="{
-                                    'active text-black border-b-[1px] border-black transition-none hover:text-black': current ===
-                                        1
-                                }">
-                                Peta Destinasi Wisata
-                            </button>
-                        </li>
-                        <li>
-                            <button @click="current = 2" class="px-6 py-2 hover:text-gray-600"
-                                x-bind:class="{
-                                    'active text-black border-b-[1px] border-black transition-none hover:text-black': current ===
-                                        2
-                                }">
-                                Peta Desa Wisata
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="mt-3 text-black">
-                    <div x-show="current == 1" x-transition:enter="transform duration-500"
-                        x-transition:enter-start="opacity-0 translate-y-6"
-                        x-transition:enter-end="opacity-100 translate-y-0" class="static block xl:flex">
-                        <div id="touristMap" class="w-full rounded-lg xl:w-3/4 h-128"></div>
-                        <div class="w-full mt-5 ml-4 space-y-4 overflow-y-auto xl:mt-0 xl:w-1/4 scrollbar h-128">
-                            @for ($i = 0; $i < 5; $i++)
-                                <div class="items-center block h-16">
-                                    <h3 class="text-lg font-semibold text-gray-500">{{ $i }}</h3>
-                                    <p class="mt-1 text-sm font-medium text-gray-500">Statistik Destinasi Wisata</p>
-                                </div>
-                            @endfor
+                    <h2 class="mb-3 text-base font-semibold">Statistik Destinasi Wisata Per Kecamatan</h2>
+                    <div class="overflow-x-scroll h-[24rem]">
+                        <div class="h-[28rem]">
+                            <canvas class="p-2" id="subDistrictChart"></canvas>
                         </div>
                     </div>
-                    <div x-show="current == 2" x-transition:enter="transform duration-500"
-                        x-transition:enter-start="opacity-0 translate-y-6"
-                        x-transition:enter-end="opacity-100 translate-y-0" class="block xl:flex">
-                        <div id="villageMap" class="w-full rounded-lg xl:w-3/4 h-128"></div>
-                        <div class="w-full ml-4 space-y-4 overflow-y-auto xl:mt-0 sm:mt-5 xl:w-1/4 scrollbar h-128">
-                            @for ($i = 0; $i < 10; $i++)
-                                <div class="items-center block h-16">
-                                    <h3 class="text-lg font-semibold text-gray-500">{{ $i }}</h3>
-                                    <p class="mt-1 text-sm font-medium text-gray-500">Statistik Desa Wisata</p>
-                                </div>
-                            @endfor
+                </div>
+                <div class="mt-10 lg:mt-0">
+                    <h2 class="mb-3 text-base font-semibold">Statistik Destinasi Wisata Per Kategori</h2>
+                    <div class="overflow-x-scroll h-[24rem]">
+                        <div class="h-[28rem]">
+                            <canvas class="p-2" id="categoryChart"></canvas>
                         </div>
                     </div>
                 </div>
@@ -121,42 +87,91 @@
     </div>
 
     @section('script')
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <script>
-            let touristMap = L.map('touristMap').setView([-8.13593475, 111.64019829777817], 11);
-
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 15,
-                minZoom: 10,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(touristMap);
-
-            function popUp(f, l) {
-                var out = [];
-                if (f.properties) {
-                    for (key in f.properties) {
-                        out.push(key + ": " + f.properties[key]);
-                    }
-                    l.bindPopup(out.join("<br />"));
-                }
-            }
-
             @foreach ($subDistricts as $subDistrict)
                 new L.GeoJSON.AJAX(['{{ asset('storage/geojson/' . $subDistrict->geojson_name) }}'], {
-                    onEachFeature: popUp,
                     style: {
                         'color': '{{ $subDistrict->fill_color }}',
                         'weight': 2,
                         'opacity': 0.4,
                     }
-                }).addTo(touristMap);
+                }).addTo(map);
             @endforeach
 
-            let villageMap = L.map('villageMap').setView([-8.13593475, 111.64019829777817], 13);
+            @foreach ($touristDestinations as $touristDestination)
+                L.marker([{{ $touristDestination->latitude }}, {{ $touristDestination->longitude }}]).addTo(map);
+            @endforeach
 
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(villageMap);
+            let jsonSubDistricts = @json($subDistricts);
+            let subDistricts = jsonSubDistricts.map((item, index) => ({
+                name: item.name,
+                count: item.tourist_destinations_count,
+                color: item.fill_color + "BF"
+            }));
+
+            new Chart(document.getElementById('subDistrictChart'), {
+                type: 'bar',
+                data: {
+                    labels: subDistricts.map(row => row.name),
+                    datasets: [{
+                        label: 'Jumlah Destinasi Wisata',
+                        data: subDistricts.map(row => row.count),
+                        borderWidth: 1,
+                        backgroundColor: subDistricts.map(row => row.color),
+                    }]
+                },
+                options: {
+                    indexAxis: 'y',
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            ticks: {
+                                color: '#000000'
+                            }
+                        }
+                    },
+                    maintainAspectRatio: false,
+                    layout: {
+                        padding: {
+                            left: 50
+                        }
+                    }
+                }
+            });
+
+            let jsonCategories = @json($categories);
+            let categories = jsonCategories.map((item, index) => ({
+                name: item.name,
+                count: item.tourist_destinations_count,
+            }));
+            const autocolors = window['chartjs-plugin-autocolors'];
+
+            const categoryChart = new Chart(document.getElementById('categoryChart'), {
+                type: 'bar',
+                data: {
+                    labels: categories.map(row => row.name),
+                    datasets: [{
+                        label: 'Jumlah Destinasi Wisata',
+                        data: categories.map(row => row.count),
+                        borderWidth: 1,
+                        backgroundColor: ['#475569BF', '#EF4444BF', '#16A34ABF', '#0284C7BF', '#DB2777BF']
+                    }]
+                },
+                options: {
+                    indexAxis: 'y',
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            ticks: {
+                                color: '#000000'
+                            },
+                        },
+
+                    },
+                    maintainAspectRatio: false,
+                }
+            });
         </script>
     @endsection
 </x-app-layout>
