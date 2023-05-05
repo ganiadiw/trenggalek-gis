b40103c7b9a51e23618fe5f3150e93376f3ac651
ganiadiw <ganiadiw@gmail.com>
2023-05-05 20:20:27 +0700

    test: add new and missing test for new icon marker field

--- tests/Feature/Category/DeleteCategoryTest.php
+++ tests/Feature/Category/DeleteCategoryTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Category;
 
 use App\Models\Category;
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class DeleteCategoryTest extends TestCase
@@ -18,6 +20,9 @@ class DeleteCategoryTest extends TestCase
     {
         parent::setUp();
 
+        $image = UploadedFile::fake()->image('icon.jpg')->hashName();
+        Storage::disk('local')->put('public/categories/icon/' . $image, '');
+
         $this->superAdmin = User::factory()->create();
         $this->webgisAdmin = User::factory()->create([
             'name' => 'Hugo First',
@@ -28,7 +33,10 @@ class DeleteCategoryTest extends TestCase
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'is_admin' => 0,
         ]);
-        $this->category = Category::factory()->create();
+        $this->category = Category::factory()->create([
+            'icon_name' => $image,
+            'icon_path' => 'public/categories/icon/' . $image,
+        ]);
     }
 
     public function test_an_superadmin_can_delete_category()
@@ -39,6 +47,17 @@ class DeleteCategoryTest extends TestCase
         $this->assertDatabaseMissing('categories', [
             'name' => $this->category->name,
         ]);
+    }
+
+    public function test_an_superadmin_can_delete_category_with_icon_marker()
+    {
+        $this->assertEquals(1, $this->superAdmin->is_admin);
+        $response = $this->actingAs($this->superAdmin)->delete('/dashboard/categories/' .  $this->category->slug);
+        $response->assertRedirect(url()->previous());
+        $this->assertDatabaseMissing('categories', [
+            'name' => $this->category->name,
+        ]);
+        $this->assertFalse(Storage::exists($this->category->icon_path));
     }
 
     public function test_an_webgis_administrator_cannot_delete_category()
--- tests/Feature/Category/UpdateCategoryTest.php
+++ tests/Feature/Category/UpdateCategoryTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Category;
 
 use App\Models\Category;
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class UpdateCategoryTest extends TestCase
@@ -18,6 +20,9 @@ class UpdateCategoryTest extends TestCase
     {
         parent::setUp();
 
+        $image = UploadedFile::fake()->image('icon.jpg')->hashName();
+        Storage::disk('local')->put('public/categories/icon/' . $image, '');
+
         $this->superAdmin = User::factory()->create();
         $this->webgisAdmin = User::factory()->create([
             'name' => 'Hugo First',
@@ -28,7 +33,10 @@ class UpdateCategoryTest extends TestCase
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'is_admin' => 0,
         ]);
-        $this->category = Category::factory()->create();
+        $this->category = Category::factory()->create([
+            'icon_name' => $image,
+            'icon_path' => 'public/categories/icon/' . $image,
+        ]);
     }
 
     public function test_a_category_edit_page_can_be_rendered()
@@ -62,6 +70,37 @@ class UpdateCategoryTest extends TestCase
         $this->assertDatabaseHas('categories', [
             'name' => 'Wisata Pantai Pesisir',
         ]);
+    }
+
+    public function test_an_superadmin_can_update_category_with_icon_marker_and_delete_old_icon_file_if_exist()
+    {
+        $this->assertEquals(1, $this->superAdmin->is_admin);
+        $response = $this->actingAs($this->superAdmin)->put('dashboard/categories/' . $this->category->slug, [
+            'name' => 'Wisata Pantai Pesisir',
+            'icon' => UploadedFile::fake()->create('icon.png', ''),
+        ]);
+        $response->assertValid(['name']);
+        $response->assertRedirect(url()->previous());
+        $category = Category::first();
+        $this->assertDatabaseMissing('categories', [
+            'name' => $this->category->name,
+        ]);
+        $this->assertDatabaseHas('categories', [
+            'name' => 'Wisata Pantai Pesisir',
+        ]);
+        $this->assertTrue(Storage::exists($category->icon_path));
+        $this->assertFalse(Storage::exists($this->category->icon_path));
+    }
+
+    public function  test_an_superadmin_can_delete_icon_marker_without_delete_the_category_itself()
+    {
+        $this->assertEquals(1, $this->superAdmin->is_admin);
+        $response = $this->actingAs($this->superAdmin)->deleteJson('dashboard/categories/delete-icon/' . $this->category->slug);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'message' => 'Delete image successfully',
+        ]);
+        $this->assertFalse(Storage::exists($this->category->icon_path));
     }
 
     public function test_an_webgis_admin_cannot_update_category()
