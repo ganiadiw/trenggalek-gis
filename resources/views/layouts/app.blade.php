f06f3d6d4342134b598c2be0abdd9e01b9114265
ganiadiw <ganiadiw@gmail.com>
2023-05-07 18:57:25 +0700

    add validation error toaster

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/nano.min.css" />
     <link rel="stylesheet" href="{{ asset('assets/css/leaflet.css') }}">
     <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" />
+    <link rel="stylesheet"
+        href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" />
     <link rel="stylesheet" href="https://unpkg.com/tippy.js@6/dist/tippy.css" />
     <style>
         [x-cloak] {
@@ -22,7 +23,8 @@
         }
     </style>
 
-    <script src="https://cdn.tiny.cloud/1/3e5fngs61v628hiv11876o7vhcv1akokhq9ybh6fihse10me/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/3e5fngs61v628hiv11876o7vhcv1akokhq9ybh6fihse10me/tinymce/6/tinymce.min.js"
+        referrerpolicy="origin"></script>
     <script src="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js"></script>
     <script src="{{ asset('assets/js/leaflet/leaflet.js') }}"></script>
     <script src="{{ asset('assets/js/leaflet/leaflet.ajax.js') }}"></script>
@@ -40,8 +42,40 @@
         <div class="hidden bg-white w-85 xl:block">
             @include('layouts.sidebar')
         </div>
-        <div class="w-full bg-gray-100">
+        <div class="relative w-full bg-gray-100">
             @include('layouts.navigation')
+            @if ($errors->any())
+                <div class="absolute flex justify-center w-full">
+                    <div id="toast-warning" class="flex items-center p-4 text-gray-500 bg-white rounded-lg shadow w-fit"
+                        role="alert">
+                        <div
+                            class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                            <span class="sr-only">Warning icon</span>
+                        </div>
+                        <div class="px-3 ml-3 text-sm font-normal">
+                            <div class="font-semibold text-red-500">Terdapat Kesalahan Nilai Masukkan</div>
+                            <div class="font-semibold text-red-500">Silahkan Cek Kembali</div>
+                        </div>
+                        <button type="button"
+                            class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500"
+                            data-dismiss-target="#toast-warning" aria-label="Close">
+                            <span class="sr-only">Close</span>
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            @endif
             <main>
                 {{ $slot }}
             </main>
