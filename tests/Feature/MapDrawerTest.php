20bc56a8f20b7a38a7fdbe24447e6a508266a9e1
ganiadiw <ganiadiw@gmail.com>
2023-05-09 21:40:53 +0700

    add new test for map drawer feature

---
+++ tests/Feature/MapDrawerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MapDrawerTest extends TestCase
+{
+    public function test_a_map_drawer_page_can_be_rendered()
+    {
+        $user = User::factory()->create();
+
+        $this->assertEquals(1, $user->is_admin);
+        $response = $this->actingAs($user)->get('/dashboard/map-drawer');
+
+        $response->assertStatus(200);
+        $response->assertSeeText('Map Drawer');
+    }
+}
