52a093501a20c5030aa4090b9908976dfe3b9bbc
ganiadiw <ganiadiw@gmail.com>
2023-05-13 23:42:16 +0700

    add custom marker field

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -18,22 +18,26 @@ class CategorySeeder extends Seeder
             [
                 'name' => 'Wisata Pantai',
                 'slug' => str()->slug('Wisata Pantai') . '-' . str()->random(5),
+                'color' => 'green',
+                'svg_name' => 'bi bi-tsunami',
             ],
             [
-                'name' => 'Wisata Bahari',
-                'slug' => str()->slug('Wisata Bahari') . '-' . str()->random(5),
-            ],
-            [
-                'name' => 'Wisata Religi',
-                'slug' => str()->slug('Wisata Religi') . '-' . str()->random(5),
+                'name' => 'Wisata Konservasi',
+                'slug' => str()->slug('Wisata Konservasi') . '-' . str()->random(5),
+                'color' => 'lightgreen',
+                'svg_name' => 'bi bi-globe-asia-australia',
             ],
             [
                 'name' => 'Wisata Agro',
                 'slug' => str()->slug('Wisata Agro') . '-' . str()->random(5),
+                'color' => 'orange',
+                'svg_name' => 'bi bi-flower3',
             ],
             [
                 'name' => 'Wisata Gunung',
                 'slug' => str()->slug('Wisata Gunung') . '-' . str()->random(5),
+                'color' => 'darkgreen',
+                'svg_name' => 'bi bi-tree-fill',
             ],
         ]);
     }
