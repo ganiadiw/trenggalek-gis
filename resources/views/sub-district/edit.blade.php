38ddf4d8d5fd37e1efcc7647b9a1b40906e686e4
ganiadiw <ganiadiw@gmail.com>
2023-05-15 22:33:40 +0700

    use error message component

--- resources/views/sub-district/edit.blade.php
+++ resources/views/sub-district/edit.blade.php
@@ -37,24 +37,7 @@
                                                 class="ml-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 px-4"
                                                 placeholder="#06b6d4" autocomplete="off">
                                         </div>
-                                        @error('fill_color')
-                                            <p id="standard_error_help"
-                                                class="flex items-center mt-2 text-xs text-yellow-700">
-                                                <span class="mr-3 font-medium">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                        class="w-[18px] h-[18px] icon icon-tabler icon-tabler-alert-triangle-filled"
-                                                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                        stroke="currentColor" fill="none" stroke-linecap="round"
-                                                        stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                        <path
-                                                            d="M11.94 2a2.99 2.99 0 0 1 2.45 1.279l.108 .164l8.431 14.074a2.989 2.989 0 0 1 -2.366 4.474l-.2 .009h-16.856a2.99 2.99 0 0 1 -2.648 -4.308l.101 -.189l8.425 -14.065a2.989 2.989 0 0 1 2.555 -1.438zm.07 14l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z"
-                                                            stroke-width="0" fill="currentColor"></path>
-                                                    </svg>
-                                                </span>
-                                                {{ $message }}
-                                            </p>
-                                        @enderror
+                                        <x-input-error-validation error="fill_color" />
                                     </div>
                                     <div>
                                         <p class="mb-2 text-sm font-semibold">Unggah Peta Kecamatan*</p>
@@ -172,29 +155,7 @@
                                                                 pada peta</button>
                                                         </div>
                                                     </div>
-                                                    @error('geojson_text_area')
-                                                        <p id="standard_error_help"
-                                                            class="flex items-center mt-2 text-xs text-yellow-700">
-                                                            <span class="mr-3 font-medium">
-                                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                                    class="icon icon-tabler icon-tabler-info-circle"
-                                                                    width="20" height="20" viewBox="0 0 24 24"
-                                                                    stroke-width="2" stroke="currentColor" fill="none"
-                                                                    stroke-linecap="round" stroke-linejoin="round">
-                                                                    <path stroke="none" d="M0 0h24v24H0z"
-                                                                        fill="none">
-                                                                    </path>
-                                                                    <circle cx="12" cy="12" r="9">
-                                                                    </circle>
-                                                                    <line x1="12" y1="8" x2="12.01"
-                                                                        y2="8">
-                                                                    </line>
-                                                                    <polyline points="11 12 12 12 12 16 13 16"></polyline>
-                                                                </svg>
-                                                            </span>
-                                                            {{ $message }}
-                                                        </p>
-                                                    @enderror
+                                                    <x-input-error-validation error="geojson_text_area" />
                                                 </div>
                                             </div>
                                         </div>
